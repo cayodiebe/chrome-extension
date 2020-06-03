b96caedc9370c50471075d998b0346406d06219a
Cayo Diebe <cayodiebe@gmail.com>
2020-06-03 18:34:36 -0300

    Inicial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Coronavirus Extension
+
+## Extensão para não ficar maluco com as notícias diárias e conseguir focar no trabalho
---
+++ script.js
@@ -0,0 +1,7 @@
+(() => {
+  document.body.innerHTML = document.body.innerHTML.
+  replace(
+    /(facebook|twitter|instagram|like|likes|curtidas|tweet|sergio moro|moro|sergiomoro|bolsonaro|jair messias bolsonaro|coronavirus|congresso|Aras|antifascistas|marginais|fake news|Mourao|mourao|pandemia|tragedia|tragédia|mortos|infectados|coronavírus|corona virus|corona vírus|infectados|George Floyd|manifestações|manifestação|bolsonarista|democracia|hidroxicloroquina|covid-19|covid19|covid|corona|messias bolsonaro|bolsonaro|carlos bolsonaro|luis inacio lula da silva|lula|partido dos trabalhadores|fakenews|dias toffoli|toffoli|paulo guedes|ministerio|ministro|)/gi,
+    ""
+  );
+})();
